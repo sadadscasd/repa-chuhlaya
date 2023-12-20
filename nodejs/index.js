467ef086b04eb9aecc7fcac2f977af42d7f34968
Razuisya <50580765+sadadscasd@users.noreply.github.com>
2023-12-20 13:01:33 +0700

    Create index.js

---
+++ nodejs/index.js
@@ -0,0 +1,8 @@
+const http = require("node:http");
+
+const server = http.createServer((req, res) => {
+res.statusCode = 200;
+res.setHeader("Content-Type", "application/json")
+res.end(JSON.stringify({message:"Hello, Docker"}));
+} );
+server.listen(5050,() => console.log("Http server started!"));
